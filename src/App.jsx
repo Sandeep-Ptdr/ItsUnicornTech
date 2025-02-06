105ab0214247130d838aa11331bd546df6a35d85
Jatin Puri <purijatinn@gmail.com>
2025-02-06 19:47:17 +0530

    add AboutPage route and new SVG assets; clean up index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import UserLayout from './layout/UserLayout'
 import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
  
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<UserLayout/>}>
             <Route index element={<HomePage />}/>
+            <Route path='about' element={<AboutPage />}/>
 
           </Route>
         </Routes>
